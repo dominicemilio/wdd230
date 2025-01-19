119e7ea6cdcc936d9c248690a334cd6c27df6c79
dominicemilio <emilio.clemence@outlook.com>
2025-01-19 18:17:50 -0500

    hamburger.js revision

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -2,10 +2,8 @@
 
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
-const map = document.querySelector('#map');
 
 hamButton.addEventListener('click', () => {
   navigation.classList.toggle('open');
   hamButton.classList.toggle('open');
-  map.classList.toggle('open');
 });
