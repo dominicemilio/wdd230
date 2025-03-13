827caaa73b5bbbbdea30e36cf456d62e566a2d52
dominicemilio <emilio.clemence@outlook.com>
2025-03-13 18:57:15 -0500

    comment

--- scripts/form.js
+++ scripts/form.js
@@ -1,11 +1,8 @@
-// ------ CHECK RE-ENTERED PASSWORD ------
 const pw1 = document.querySelector("#password");
 const pw2 = document.querySelector("#password2");
 const message = document.querySelector("#match-msg");
 
-// pw1.addEventListener("focusout", checkSame);
 pw2.addEventListener("focusout", checkSame);
-// pw2.addEventListener("input", checkSame);
 
 function checkSame() {
 	if (pw1.value !== pw2.value) {
@@ -21,16 +18,12 @@ function checkSame() {
 	}
 }
 
-
-
-// ------ DISPLAY RATING VALUE ------
 const rangevalue = document.querySelector("#rangevalue");
 const range = document.querySelector("#ratingRange");
 
-// RANGE event listener
 range.addEventListener('change', displayRatingValue);
 range.addEventListener('input', displayRatingValue);
 
 function displayRatingValue() {
-    rangevalue.innerHTML = range.value;
+	rangevalue.innerHTML = range.value;
 }
